b5a3e8d3066ea90b9baf4976651ac7ba93daa691
FireBuddy <aijun@live.ca>
2016-08-10 13:33:45 -0400

    Update Modes.cs

--- Godlike Vel'Koz/Modes.cs
+++ Godlike Vel'Koz/Modes.cs
@@ -169,7 +169,7 @@ namespace Godlike_Vel_Koz
             if (Manager.killstealQ)
             {
                 var target = Target.GetChampionTarget(Spells.Q.Range, DamageType.Magical, false, true, Spells.QDamage());  
-                if (target != null)
+                if (target != null && Player.Instance.Spellbook.GetSpell(SpellSlot.Q).ToggleState == 1)
                     Spells.CastQ(target);
             }
             if (Manager.killstealW)
