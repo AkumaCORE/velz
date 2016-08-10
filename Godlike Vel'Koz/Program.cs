cc722e08beae46f4cf144952a010bb08804b9b09
FireBuddy <aijun@live.ca>
2016-08-10 14:16:53 -0400

    Update Program.cs

--- Godlike Vel'Koz/Program.cs
+++ Godlike Vel'Koz/Program.cs
@@ -117,7 +117,7 @@ namespace Godlike_Vel_Koz
 
             {
                  // Chat.Print("Q detected");
-                  var Direction = (Handle.EndPosition.To2D() - Handle.StartPosition.To2D()).Normalized();
+                  Direction = (Handle.EndPosition.To2D() - Handle.StartPosition.To2D()).Normalized();
                   Perpendiculars.Add(Direction.Perpendicular());
                   Perpendiculars.Add(Direction.Perpendicular2());
 
