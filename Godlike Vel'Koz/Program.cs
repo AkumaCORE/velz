1de123bb901a057d2f7c227b302228b6be64c527
FireBuddy <aijun@live.ca>
2016-08-10 09:18:20 -0400

    Update Program.cs

--- Godlike Vel'Koz/Program.cs
+++ Godlike Vel'Koz/Program.cs
@@ -20,7 +20,7 @@ using Mario_s_Lib;
         #endregion Sounds
 
         public static AIHeroClient Champion { get { return Player.Instance; } }
-        private static List<Vector2> Perpendiculars { get; set; }
+        public static List<Vector2> Perpendiculars { get; set; }
         static int playerKills = 0;
         
         //private static MissileClient QMissile;
